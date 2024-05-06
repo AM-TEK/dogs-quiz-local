24bbe19e9718b501104669fd01cb75c9f6e4053e
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-05-06 12:17:40 -0500

    adjust handleAnswer, add handleSubmit to send selections to backend, and tailwind classes

--- dogs-quiz-local-frontend/src/app/page.js
+++ dogs-quiz-local-frontend/src/app/page.js
@@ -4,7 +4,8 @@ import DogList from "@/components/DogList";
 export default function Home() {
   return (
     <div>
-      <h1>Dog Quiz</h1>
+      <h1>Pet Dogs</h1>
+      
       <h3>Choose the dog breed based off the picture: </h3>
       <DogList />
     </div>
--- dogs-quiz-local-frontend/src/components/DogList.js
+++ dogs-quiz-local-frontend/src/components/DogList.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 function DogList() {
   const [dogs, setDogs] = useState([]);
+  const [userAnswers, setUserAnswers] = useState({});
+  const [score, setScore] = useState(null);
 
   useEffect(() => {
     axios.get('http://localhost:8080/dogs')
@@ -16,31 +18,65 @@ function DogList() {
   }, []);
 
   const handleAnswer = (dogId, selectedOption) => {
-    const selectedDog = dogs.find(dog => dog.id === dogId);
-    if (selectedDog.answer === selectedOption) {
-      alert('Correct!');
-    } else {
-      alert('Incorrect. The correct answer is ' + selectedDog.answer);
-    }
-  };
+    setUserAnswers(prevState => ({
+        ...prevState,
+        [dogId]: selectedOption
+    }));
+};
+
+const handleSubmit = () => {
+    axios.post('http://localhost:8080/score', userAnswers)
+        .then(response => {
+            setScore(response.data.score);
+        })
+        .catch(error => {
+            console.error('Error calculating score:', error);
+        });
+};
 
   return (
-    <div>
-      <h1>Dogs Quiz</h1>
-      <ul>
-        {dogs && dogs.map(dog => (
-          <li key={dog.id}>
-            <img src={dog.image} alt={dog.breed} />
+    <div className="flex">
+        <div className="w-1/2">
+            <h1 className='text-2xl font-bold m-4'>Dogs Quiz</h1>
+                <ul>
+                    {dogs && dogs.map(dog => (
+                        <li key={dog.id}>
+                            <img src={dog.image} alt={dog.breed} className='m-4 rounded-lg' />
+                            <ul>
+                                {dog.options.map(option => (
+                                    <li key={option}>
+                                        <button 
+                                            onClick={() => handleAnswer(dog.id, option)}
+                                            className={`border mx-4 my-2 p-2 rounded-md ${userAnswers[dog.id] === option ? 'border-blue-500' : 'border-gray-300'}`}
+                                            >
+                                            {option}
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+                <button 
+                    onClick={handleSubmit}
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-4 rounded-md'
+                    >
+                    Submit Answers
+                </button>
+            {score !== null && <p className='mt-4'>Score: {score}</p>}
+        </div>
+        
+        <div className="w-1/2">
+            <h2 className='text-2xl font-bold m-4'>Dog Images and Breeds</h2>
             <ul>
-              {dog.options.map(option => (
-                <li key={option}>
-                  <button onClick={() => handleAnswer(dog.id, option)}>{option}</button>
-                </li>
-              ))}
+                {dogs && dogs.map(dog => (
+                    <li key={dog.id} className="flex items-center space-x-4 p-2">
+                    <img src={dog.image} alt={dog.breed} className='rounded-lg w-32 h-32' />
+                    <span className="text-lg">{dog.breed}</span>
+                    </li>
+                ))}
             </ul>
-          </li>
-        ))}
-      </ul>
+      </div>
     </div>
   );
 }
