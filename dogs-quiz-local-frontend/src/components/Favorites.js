14a3433f981c8e07fc21448c590db7c7616b80c9
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-05-08 13:46:37 -0500

    added dog card to display dog data when selected in favorites section

--- dogs-quiz-local-frontend/src/components/Favorites.js
+++ dogs-quiz-local-frontend/src/components/Favorites.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SelectedDogCard from './SelectedDogCard';
 
 function Favorites({ favorites, quizSubmitted }) {
+  const [selectedDog, setSelectedDog] = useState(null);
+
+  const handleClick = (dog) => {
+    setSelectedDog(dog);
+  };
+
   return (
     <div className="w-1/2">
       <h2 className='text-2xl font-bold m-4'>Favorites</h2>
@@ -10,11 +17,17 @@ function Favorites({ favorites, quizSubmitted }) {
       <ul>
         {favorites.map(dog => (
           <li key={dog.id} className="flex items-center space-x-4 p-2">
-            <img src={dog.image} alt={dog.breed} className='rounded-lg w-32 h-32' />
+            <img
+              src={dog.image}
+              alt={dog.breed}
+              className='rounded-lg w-32 h-32 cursor-pointer'
+              onClick={() => handleClick(dog)}
+            />
             <span className="text-lg">{dog.breed}</span>
           </li>
         ))}
       </ul>
+      {selectedDog && <SelectedDogCard dog={selectedDog} />}
     </div>
   );
 }
